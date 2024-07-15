dceb8ce746589b17abf1cc33717b6d93e90576eb
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2024-07-15 09:35:29 +0330

    Caesar Version 1.3
    Interface for standard and advanced mode
    Support all lower and upper characters
    Simple Documentation

--- Caesar.py
+++ Caesar.py
@@ -5,7 +5,7 @@ import sys
 Encrypts a string using Caesar Cipher.
 
 Author: NightFox
-Version: 1.2
+Version: 1.3
 TimeStamp: 1720999524.7264638
 """
 
@@ -23,14 +23,15 @@ def safe_key(status=True):
         print('SafeKey is Disable.')
 
 
-def overflow(value: int):
-    if value > 52:
-        return value % 52
+def overflow(key: int):
+    if key > 52:
+        return key % 52
     else:
-        return value
+        return key
 
 
 def shift(key: int):
+    """Create Shifted/Cipher Dictionary"""
     k = overflow(key)
     letters = list(string.ascii_lowercase + string.ascii_uppercase)
 
@@ -47,6 +48,7 @@ def shift(key: int):
 
 
 def str_to_str(value: str, key: int):
+    """Generates String from Caesar Cipher Dictionary"""
     letters = list(string.ascii_lowercase + string.ascii_uppercase)
     _shift = shift(key)
     _list = [_shift[letters.index(i)] if i in letters else i for i in value]  # ignore un-ascii character
@@ -54,24 +56,58 @@ def str_to_str(value: str, key: int):
 
 
 def str_shift(value: str, key: int):
+    """Caesar Cipher: the Shift compute"""
     k = overflow(key)
     return str_to_str(value, k)  # Shift 5: hello -> czggj
 
 
 def str_unshift(value: str, key: int):
+    """Caesar Cipher: the Unshift compute"""
     k = 52 - overflow(key)
     return str_to_str(value, k)  # Unshift 5: czggj -> hello
 
 
 # ORD
-def str_to_ord(value, **kwargs):
+def str_to_ord(value: str, **kwargs):
+    """Converts a string to an ord() integer"""
     # list of ord 's
     o = [str(ord(_chr)).zfill(3) for _chr in str(value)]  # 'h' : '104' | length=3
     return ''.join(o) + '0'  # input: str > output: str
 
 
 # CHR
-def int_to_chr(value, **kwargs):
+def int_to_chr(value: str, **kwargs):
+    """Converts an ord() integer to chr() string"""
     # list of chr 's
     c = [chr(int(_ord)) for _ord in [value[i:i + 3] for i in range(0, len(value) - 2, 3)]]  # '104' : 'h' | length=3
     return ''.join(c)  # input: str > output: str
+
+
+def caesar_encryption(value: str, key: int):
+    """Caesar Cipher Standard Encryption Interface."""
+    return str_shift(value=value, key=key)
+
+
+def caesar_decryption(value: str, key: int):
+    """Caesar Cipher Standard Decryption Interface."""
+    return str_unshift(value=value, key=key)
+
+
+def advance_encryption(value: str, key: int):
+    """
+    Advanced Cipher Encryption Interface
+    Using Caesar Cipher and ord functionality.
+    """
+    _cea = caesar_encryption(value, key)
+    _ord = str_to_ord(_cea)
+    return _ord
+
+
+def advance_decryption(value: str, key: int):
+    """
+    Advanced Cipher Decryption Interface
+    Using Caesar Cipher and ord functionality.
+    """
+    _chr = int_to_chr(value)
+    _cea = caesar_decryption(_chr, key)
+    return _cea
